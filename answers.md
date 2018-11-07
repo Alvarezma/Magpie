1aa5ae9046f3c7e8786cce81640289e1a54105fb
Alvarezma <Derpachu@comcast.net>
2018-11-07 08:43:46 -0700

    Exercises finished

--- answers.md
+++ answers.md
@@ -3,3 +3,6 @@ Exploration -
     I want to understand French - What would it mean to understand French?
     Do you like me? - What makes you think that I like you?
     You confuse me. - What makes you think that I confuse you?
+Exercises - 
+    I really love bagels do you love them too - Why do you really love bagels do me?
+    More complex sentences like this can trigger a response that isn't made for something like this. The system depends on the user making simple sentences. We could change it to see the ammount of words between the two keywords and decided whether or not they are close enough to use the simple statment response.
--- src/Magpie4.java
+++ src/Magpie4.java
@@ -46,15 +46,20 @@ public class Magpie4 {
 		// Responses which require transformations
 		else if (findKeyword(statement, "I want to", 0) >= 0) {
 			response = transformIWantToStatement(statement);
+		} else if (findKeyword(statement, "I want", 0) >= 0)	{
+			response = transformIWantStatement(statement);
 		}
 
 		else {
 			// Look for a two word (you <something> me)
 			// pattern
 			int psn = findKeyword(statement, "you", 0);
+			int psn2 = findKeyword(statement, "i", 0);
 
 			if (psn >= 0 && findKeyword(statement, "me", psn) >= 0) {
 				response = transformYouMeStatement(statement);
+			} else if (psn2 >= 0 && findKeyword(statement, "you", psn2) >= 0) {
+				response = transformIYouStatement(statement);
 			} else {
 				response = getRandomResponse();
 			}
@@ -82,6 +87,18 @@ public class Magpie4 {
 		return "What would it mean to " + restOfStatement + "?";
 	}
 
+	private String transformIWantStatement(String statement) {
+		// Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
+	}
+
 	/**
 	 * Take a statement with "you <something> me" and transform it into
 	 * "What makes you think that I <something> you?"
@@ -102,8 +119,24 @@ public class Magpie4 {
 		int psnOfMe = findKeyword(statement, "me", psnOfYou + 3);
 
 		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
-		
+
 		return "What makes you think that I " + restOfStatement + " you?";
+	}
+
+	private String transformIYouStatement(String statement) {
+		// Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+
+		int psnOfI = findKeyword(statement, "i", 0);
+		int psnOfYou = findKeyword(statement, "you", psnOfI + 1);
+
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+
+		return "Why do you " + restOfStatement + " me?";
 	}
 
 	/**
